7deb56c680518c7b038ac6c87eca95e035976e20
AbhiDoshi2000 <abhidoshi2000@gmail.com>
2024-07-11 16:25:59 -0500

    Add my ready website content

---
+++ src/constants.js
@@ -0,0 +1,48 @@
+
+export const services = [
+    {
+        title: "Software Engineer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Traveller",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Sports Enthusiast",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+];
+
+export const name = 'Abhishek';
+
+export const experiences = [
+    {
+        'company': 'Caypro',
+        'role': 'Software Engineer Intern',
+        'duration': 'Dec 2021 - Apr 2022',
+        'logo': 'https://media.licdn.com/dms/image/C4D0BAQGJNms_rAb_Qw/company-logo_200_200/0/1630545901277/talentnow_solution_service_pvt_ltd_logo?e=2147483647&v=beta&t=Ips5gXvYZNteu0fBOLbMqSBT4UiqCSqNnVw3pDcsln8',
+        'points': [
+            'Built a Recruitment Chatbot using NLP techniques and ML libraries (Gensim, spaCy, TensorFlow). Streamlined hiring, reducing time-to-fill by 40%, and increasing candidate engagement by 50%.',
+            'Led a Resume Parser project, utilizing OCR and advanced NLP (NLTK, Scikit-learn, PyTorch) for extracting vital applicant details, aligning with company requirements. Reduced manual effort by 70%, showcasing proficiency in diverse ML technologies.',
+        ],
+        'url': 'https://www.caypro.io/',
+    },
+    {
+        'company': 'Orbit Tours and Travels',
+        'role': 'Android Developer',
+        'duration': 'Jun 2020 - Nov 2021',
+        'logo': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSjC47R2X0_Apg6PjHuZohpF_XZ6da0mvyfw&s',
+        'points': [
+            'Built an android app using Java, Android Studio and Google Firebase. Integrated features like flight booking, tour management, forex services, and passport/visa assistance, resulting in a significant increase in customer engagement by over 30%..',
+            'Performed comprehensive functional and web automation testing with the Appium framework.'
+        ],
+        'url': 'https://www.facebook.com/orbittoursandtravels/',
+    },
+]
+
+
+
+export const EMAIL_JS_SERVICE_ID = 'service_ifnovy8';
+export const EMAIL_JS_TEMPLATE_ID = 'template_bxrlyz8';
+export const EMAIL_JS_PUBLIC_KEY = '3gyE0ZFr-myjQES5b';
