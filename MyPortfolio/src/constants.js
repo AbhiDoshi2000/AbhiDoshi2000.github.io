9a2c064706dddd57771351f60dafc7afdb84e711
AbhiDoshi2000 <abhidoshi2000@gmail.com>
2024-11-17 21:28:55 -0600

    intial portfolio

---
+++ MyPortfolio/src/constants.js
@@ -0,0 +1,63 @@
+
+export const services = [
+    {
+        title: "Software Engineer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Traveller",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Sports Enthusiast",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+];
+
+export const name = 'Abhishek';
+
+export const experiences = [
+    {
+        'company': 'University of Texas at Arlington',
+        'role': 'Engineering Research Assistant',
+        'duration': 'Aug 2024 - Present',
+        'logo': 'https://upload.wikimedia.org/wikipedia/en/thumb/2/29/University_of_Texas_at_Arlington_seal.svg/1200px-University_of_Texas_at_Arlington_seal.svg.png',
+        'points': [
+            'Developed distributed, cloud-native systems on Google Cloud Platform (GCP), leveraging services like Compute Engine and Cloud Functions to ensure scalability and resilience in handling large user traffic and data workloads.',
+            'Created scalable Node.js RESTful microservices and ensured high-performance for global users.',
+            'Collaborated with cross-functional teams in an Agile environment to implement distributed cloud solutions, writing technical specifications and unit tests with over 80% code coverage.'
+        ],
+        'url': 'https://www.uta.edu/',
+    },
+    {
+        'company': 'Caypro',
+        'role': 'Software Engineer Intern',
+        'duration': 'Dec 2021 - Apr 2022',
+        'logo': 'https://media.licdn.com/dms/image/C4D0BAQGJNms_rAb_Qw/company-logo_200_200/0/1630545901277/talentnow_solution_service_pvt_ltd_logo?e=2147483647&v=beta&t=Ips5gXvYZNteu0fBOLbMqSBT4UiqCSqNnVw3pDcsln8',
+        'points': [
+            'Designed and implemented hybrid mobile applications and web solutions using React, Node.js, and AWS Lambda.',
+            'Built microservices using Node.js for high-performance, scalable cloud applications.',
+            'Utilized NoSQL databases like DynamoDB & MongoDB to manage large volumes of unstructured data, used caching systems such as Redis & Elasticache to enhance application performance and minimize data retrieval latency.'
+        ],
+        'url': 'https://www.caypro.io/',
+    },
+    {
+        'company': 'Orbit Tours and Travels',
+        'role': 'Software Development Engineer',
+        'duration': 'Jun 2020 - Nov 2021',
+        'logo': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSjC47R2X0_Apg6PjHuZohpF_XZ6da0mvyfw&s',
+        'points': [
+            'Developed a real-time Android app using Java and Google Firebase, incorporating user authentication and data synchronization. Implemented RESTful APIs for seamless integration between mobile and web applications.',
+            'Worked on back-end services and databases using Node.js, TypeScript, and MySQL.',
+            'Implemented automated unit tests using Mocha and Jest, and used Google Analytics for monitoring user activity.'
+        ],
+        'url': 'https://www.facebook.com/orbittoursandtravels/',
+    },
+    
+]
+
+
+
+export const EMAIL_JS_SERVICE_ID = 'service_ifnovy8';
+export const EMAIL_JS_TEMPLATE_ID = 'template_bxrlyz8';
+export const EMAIL_JS_PUBLIC_KEY = '3gyE0ZFr-myjQES5b';
